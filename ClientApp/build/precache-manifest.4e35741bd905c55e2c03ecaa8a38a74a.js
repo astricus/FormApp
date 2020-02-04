e60c81e98d2210344304b9dbf38900a7df9ea784
astricus <astricus@hotmail.com>
2020-02-04 15:18:13 +0300

    adding /build folder

---
+++ ClientApp/build/precache-manifest.4e35741bd905c55e2c03ecaa8a38a74a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf58e183e0be862482702c85dab2907a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "324cff82c882fc59ebfd",
+    "url": "/static/js/2.6bd7175b.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/static/js/2.6bd7175b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3ddaf09f086787424215",
+    "url": "/static/js/main.5369d005.chunk.js"
+  },
+  {
+    "revision": "9a27d443797a9bf06c51",
+    "url": "/static/js/runtime-main.276dd410.js"
+  }
+]);
