af54e2be4c7e15c2c5a9fda5e1ba1cba81dbe1ba
astricus <astricus@hotmail.com>
2020-02-04 15:12:42 +0300

    updating links in copyright comonent

--- ClientApp/src/components/copyright/copyright.component.jsx
+++ ClientApp/src/components/copyright/copyright.component.jsx
@@ -6,7 +6,7 @@ const Copyright = () => {
     return (
         <Typography variant="body2" color="textSecondary" align="center">
             {'Copyright © '}
-            <Link color="inherit" href="https://material-ui.com/">
+            <Link color="inherit" href="https://github.com/astricus/FormApp">
                 FormApp
             </Link>{' '}
             {new Date().getFullYear()}
